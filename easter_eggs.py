da8f0749387f01fc8427047095111df586d6dbf4
Jay Sueno <jay.sueno@gmail.com>
2021-01-11 23:12:47 -0800

    easter egg py file

---
+++ easter_eggs.py
@@ -0,0 +1,15 @@
+# Fun easter eggs. Run the code and see what comes up. :)
+import antigravity
+import this
+
+jay = "I will find a cool job"
+
+# print(jay[:3])
+
+x = 1
+
+for letter in jay:
+    new = letter + f' Jay {x}'
+    x += 1
+    print(new)
+
